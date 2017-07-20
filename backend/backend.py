74ac9f4d47d4845d1f7e8e03b3920feeea13a13e
Hanyu Yang <yanghanyugg@gmail.com>
2017-07-19 20:26:06 -0700

    Modify API to track?meta=song/artist/album

--- backend/backend.py
+++ backend/backend.py
@@ -3,7 +3,7 @@ import requests
 import base64
 import json
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
 spotify = {}
 
 with open("spotify.txt") as f:
@@ -21,21 +21,29 @@ def getToken():
     return response["access_token"]
 
 
-@app.route("/index", methods=['GET'])
+@app.route("/index", methods=["GET"])
 def handleIndex():
     return "ok"
 
 
-@app.route("/song", methods=['GET'])
+@app.route("/track", methods=["GET"])
 def handleSong():
-    # test = request.args.get('test')
+    meta = request.args.get("meta")
     token = getToken()
     r = requests.get("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V", 
         headers = {"Authorization": "Bearer " + token})
-    return json.dumps(r.json())
-    
+    response = r.json()
+    data = ""
+    if meta == "song":
+        data = response["name"]
+    if meta == "artist":
+        data = response["artists"][0]["name"]
+    if meta == "album":
+        data = response["album"]["name"]
+    ret = {"preview": response["preview_url"], "meta": data}
+    return json.dumps(ret)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80, threaded=True)
     
