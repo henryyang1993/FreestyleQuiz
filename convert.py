7f2dbebda14d08b516def1f46b85ffe5c7b6fdf5
Daren Ye <ydaren@amazon.com>
2017-07-21 10:29:18 -0700

    Add converter

---
+++ convert.py
@@ -0,0 +1,7 @@
+import urllib
+import os
+
+def covertSong(url):
+	urllib.urlretrieve(url, "newsong")
+	command = "ffmpeg -i newsong -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 <output.mp3>"
+	os.system(command)
